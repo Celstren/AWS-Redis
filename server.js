e2869133dd911aeb464043dabdb9bfd73d94c986
Gustavo Carrillo <gustavo.carrillo.rueda10@gmail.com>
2020-10-26 00:14:13 -0500

    uncomment

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 console.log("Running");
 
-// var RedisClustr = require('redis-clustr');
-// var RedisClient = require('redis');
-// var config = require("./config.json");
+var RedisClustr = require('redis-clustr');
+var RedisClient = require('redis');
+var config = require("./config.json");
 
-// var redis = new RedisClustr({
-//     servers: [
-//         {
-//             host: config.redisClusterHost,
-//             port: config.redisClusterPort
-//         }
-//     ],
-//     createClient: function (port, host) {
-//         // this is the default behaviour
-//         return RedisClient.createClient(port, host);
-//     }
-// });
+var redis = new RedisClustr({
+    servers: [
+        {
+            host: config.redisClusterHost,
+            port: config.redisClusterPort
+        }
+    ],
+    createClient: function (port, host) {
+        // this is the default behaviour
+        return RedisClient.createClient(port, host);
+    }
+});
 
-// //connect to redis
-// redis.on("connect", function () {
-//   console.log("connected");
-// });
+//connect to redis
+redis.on("connect", function () {
+  console.log("connected");
+});
 
-// //check the functioning
-// redis.set("framework", "AngularJS", function (err, reply) {
-//   console.log("redis.set " , reply);
-// });
+//check the functioning
+redis.set("framework", "AngularJS", function (err, reply) {
+  console.log("redis.set " , reply);
+});
 
-// redis.get("name", function (err, reply) {
-//   console.log("redis.get ", reply);
-// });
+redis.get("name", function (err, reply) {
+  console.log("redis.get ", reply);
+});
