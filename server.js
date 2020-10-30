cf7c1e1fcdaf3b4c947f68968073cc2eb917756a
Gustavo Carrillo <gustavo.carrillo.rueda10@gmail.com>
2020-10-29 23:16:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,39 +22,6 @@ redis.on("connect", function () {
   console.log("connected");
 });
 
-redis.on("error", function () {
-  console.log("connected");
-});
+var a = redis.get('value');
 
-redis.on("message", function (channel, message) {
-  console.log(channel);
-  console.log(message);
-});
-
-redis.subscribe("my-channel", function (err) {
-  redis.publish("my-channel", "have a lovely day!");
-});
-
-//check the functioning
-redis.set("test", "val", function (err) {
-  if (err) {
-    // Something went wrong
-    console.error("error");
-  } else {
-    redis.get("test", function (err, value) {
-      if (err) {
-        console.error("error");
-      } else {
-        console.log("Worked: " + value);
-      }
-    });
-  }
-});
-
-redis.get("name", function (err, reply) {
-  if (err) {
-    console.error("error");
-  } else {
-    console.log("Worked: " + reply);
-  }
-});
+console.log(a);
