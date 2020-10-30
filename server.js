7e269a4a72e82cefe63d8a3c83c9f83671eefd88
Gustavo Carrillo <gustavo.carrillo.rueda10@gmail.com>
2020-10-29 23:46:10 -0500

    another approach

--- server.js
+++ server.js
@@ -1,24 +1,55 @@
-console.log("Running");
-
-var RedisClustr = require("redis-clustr");
-var RedisClient = require("redis");
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
 var config = require("./config.json");
 
-var redis = new RedisClustr({
-  servers: [
-    {
-      host: config.redisClusterHost,
-      port: config.redisClusterPort,
-    },
-  ],
-  createClient: function (port, host) {
-    // this is the default behaviour
-    return RedisClient.createClient(port, host);
-  },
+const PORT = 3000;
+const REDIS_PORT = config.redisClusterPort;
+const REDIS_HOST = config.redisClusterHost;
+
+const redisClient = redis.createClient(REDIS_PORT, REDIS_HOST)
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
 
-//connect to redis
-redis.on("connect", function () {
-  console.log("connected");
-  redis.set('test', 'test');
+async function getPublicReposNumber(req, res, next) {
+  try {
+    console.log("Fetching data...");
+
+    const { username } = req.params;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+
+    redisClient.setex(username, 3600, data);
+
+    res.satus(200).send(setResponse(username, data));
+  } catch (error) {
+    console.error(error);
+    req.status(500).json({error: error});
+  }
+}
+
+function cache(req, res, next) {
+  const { username } = req.params;
+  redisClient.get(username, function (error, cachedData) {
+    if (error) throw error;
+    if (cachedData != null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  })
+}
+
+app.get('/repos/:username', cache, getPublicReposNumber);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 });
+
+/* 
+host: config.redisClusterHost,
+      port: config.redisClusterPort,
+*/
