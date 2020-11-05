ce55b5d5e752ea996d4b389500270b17ab34e9e4
Gustavo Carrillo <gustavo.carrillo.rueda10@gmail.com>
2020-11-05 07:50:31 -0500

    socket implementation

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fetch = require("node-fetch");
 const redis = require("redis");
 var config = require("./config.json");
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 const PORT = 3000;
 const REDIS_PORT = config.redisClusterPort;
@@ -11,46 +13,52 @@ const redisClient = redis.createClient(REDIS_PORT, REDIS_HOST)
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/index.html');
 });
 
-async function getPublicReposNumber(req, res, next) {
-  try {
-    console.log("Fetching data...");
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+      console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+  });
+});
 
-    const { username } = req.params;
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    const data = JSON.stringify(await response.json());
-    console.log(`Fetched data ${data}`)
 
-    redisClient.setex(username, 3600, data);
-
-    res.send(`Saved successfully`);
-  } catch (error) {
-    console.error(error);
-    res.send({error: error});
-  }
-}
-
-function cache(req, res, next) {
-  const { username } = req.params;
-  redisClient.get(username, function (error, cachedData) {
-    if (error) throw error;
-    if (cachedData != null) {
-      res.send(`Data from cache ${cachedData}`);
-    } else {
-      next();
-    }
-  })
-}
-
-app.get('/repos/:username', cache, getPublicReposNumber);
-
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 });
 
-/* 
-host: config.redisClusterHost,
-      port: config.redisClusterPort,
-*/
+// async function getPublicReposNumber(req, res, next) {
+//   try {
+//     console.log("Fetching data...");
+
+//     const { username } = req.params;
+//     const response = await fetch(`https://api.github.com/users/${username}`);
+//     const data = JSON.stringify(await response.json());
+//     console.log(`Fetched data ${data}`)
+
+//     redisClient.setex(username, 3600, data);
+
+//     res.send(`Saved successfully`);
+//   } catch (error) {
+//     console.error(error);
+//     res.send({error: error});
+//   }
+// }
+
+// function cache(req, res, next) {
+//   const { username } = req.params;
+//   redisClient.get(username, function (error, cachedData) {
+//     if (error) throw error;
+//     if (cachedData != null) {
+//       res.send(`Data from cache ${cachedData}`);
+//     } else {
+//       next();
+//     }
+//   })
+// }
+
+// app.get('/repos/:username', cache, getPublicReposNumber);
